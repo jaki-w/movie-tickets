490475c00900c5f1440014c443a3911bdc382e88
Jaki Widby <jaki.widby@gmail.com>
2020-01-28 21:30:13 -0800

    create receipt prototype with if statements to determine price

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,23 @@ function Ticket(movie, time, age) {
 }
 
 Ticket.prototype.receipt = function() {
-
+  var total;
+  if ($("option").attr("class" == "new-release")) {
+    total = 14
+  } else if ($("option").attr("class" == "standard")) {
+    total = 12
+  }
+  if (this.time == "early-bird") {
+    total = total - 4
+  }
+  else if (this.time == "matinee") {
+    total = total - 2
+  }
+  else if (this.time == "prime-time") {
+    total = total
+  }
+  else (this.age <= 12 || this.age >= 65) {
+    total = total - 4
+  }
+  return total;
 }
